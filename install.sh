7de5015cec3be11fe0621aeb0a02552828475f96
dormirj <81703220+dormirj@users.noreply.github.com>
2021-08-22 10:46:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#I know this might be a bit lazy, just be sure that you have a proper supercollider installation and a working jack configuration
 echo "Nothing to do for now! Thank you!"
